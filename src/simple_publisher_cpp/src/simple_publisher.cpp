6c6f3b3ee5c729dffdbad7647bab6ad2e86974c0
Rohit Thampy <rohitthampy808@gmail.com>
2025-08-27 13:05:03 +0100

    First Commit

---
+++ src/simple_publisher_cpp/src/simple_publisher.cpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+
+int main(int argc, char *argv[])
+{ 
+    rclcpp::init(argc, argv);
+    return 0;
+}
