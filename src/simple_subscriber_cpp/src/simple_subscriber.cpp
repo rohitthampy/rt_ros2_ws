824f06efffca052ec327ee85b1f2a87880646479
Rohit Thampy <rohitthampy808@gmail.com>
2025-08-28 15:08:45 +0100

    Implements the simple_subscriber package

--- src/simple_subscriber_cpp/src/simple_subscriber.cpp
+++ src/simple_subscriber_cpp/src/simple_subscriber.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <memory>
 
-int main()
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
+
+class SimpleSubscriber : public rclcpp::Node
 {
-    for (int i{0}; i < 6; i++)
-        std::cout << i << std::endl;
+  public:
+    SimpleSubscriber()
+    : Node("simple_subscriber")
+    {
+      subscription_ = this->create_subscription<std_msgs::msg::String>
+      ("topic", 10, std::bind(&SimpleSubscriber::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const std_msgs::msg::String & msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "Heard: '%s'", msg.data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SimpleSubscriber>());
+  rclcpp::shutdown();
+  return 0;
 }
