6571fa57940686379a573837705d5bbed8510364
Rohit Thampy <rohitthampy808@gmail.com>
2025-08-28 12:07:11 +0100

    Completed implimentation of a simple publisher based on rclcpp `member_function.cpp` example

--- src/simple_publisher_cpp/src/simple_publisher.cpp
+++ src/simple_publisher_cpp/src/simple_publisher.cpp
@@ -2,14 +2,41 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <iostream>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+using namespace std::chrono_literals;
 
+class SimplePublisher: public rclcpp::Node
+{
+    public:
+        SimplePublisher()
+            :Node("simple_publisher"), count_(0)
+            {
+                publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+                timer_ = this->create_wall_timer(500ms, std::bind(&SimplePublisher::timer_callback, this));
+            }
+    
+    private:
+    
+        void timer_callback()
+        {
+            auto message = std_msgs::msg::String();
+            message.data = "Hello , World! " + std::to_string(count_++);
+            RCLCPP_INFO(this->get_logger(), "Publishing: %s", message.data.c_str());
+            publisher_->publish(message);
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+        size_t count_;
+};
 
 int main(int argc, char *argv[])
 { 
     rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SimplePublisher>());
+    rclcpp::shutdown();
     return 0;
 }
